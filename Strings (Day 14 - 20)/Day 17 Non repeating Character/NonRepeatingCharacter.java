009301f211d0c71c673ad7a744042451dde06975
aryangupta1105 <husltegamingfanclub@gmail.com>
2025-05-19 11:27:18 +0530

    gfg 160days challenge Day 17 find first non repeating character

---
+++ Strings (Day 14 - 20)/Day 17 Non repeating Character/NonRepeatingCharacter.java
@@ -0,0 +1,31 @@
+import java.lang.*;
+import java.util.LinkedHashMap;
+
+
+public class NonRepeatingCharacter {
+
+    static char findNonRepeatingCharacter(String s){
+        LinkedHashMap<Character, Integer> h = new LinkedHashMap<>();
+
+        for(int i = 0; i<s.length(); i++){
+            h.put(s.charAt(i), h.getOrDefault(s.charAt(i) , 0 )+1);
+        }
+        System.out.println(h);
+
+        for (Character key : h.keySet()) {
+            if(h.get(key) == 1){    
+                return key;
+            }
+        }
+        
+        
+        return '$';
+    }
+
+
+
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(findNonRepeatingCharacter(s));
+    }
+}
