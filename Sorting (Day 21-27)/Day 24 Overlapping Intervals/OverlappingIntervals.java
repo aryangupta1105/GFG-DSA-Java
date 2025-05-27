173606547bf279ff383866c167b2c23c4f21749a
aryangupta1105 <husltegamingfanclub@gmail.com>
2025-05-27 09:31:48 +0530

    gfg 160days challenge - Sorting - Day 24 overlappingintervals

---
+++ Sorting (Day 21-27)/Day 24 Overlapping Intervals/OverlappingIntervals.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OverlappingIntervals {
+
+    static List<int[]> OverlappingIntervals(int arr[][]){
+        Arrays.sort(arr , (a,b)-> Integer.compare(a[0] , b[0]));
+        
+        List<int[]> result = new ArrayList<>();
+        
+        int[] current = arr[0];
+        
+        for(int i = 1; i<arr.length; i++){
+            if(current[1] >= arr[i][0]){
+                current[1] = Math.max(current[1], arr[i][1]);
+            }
+            else{
+                result.add(current);
+                current = arr[i];
+            }
+        }
+        result.add(current);
+        
+        return result;
+    
+    }
+    
+    public static void main(String args[]){
+        int arr[][] = {{1,3},{2,4},{6,8}, {9,10}};
+
+        OverlappingIntervals(arr);
+    }
+}
