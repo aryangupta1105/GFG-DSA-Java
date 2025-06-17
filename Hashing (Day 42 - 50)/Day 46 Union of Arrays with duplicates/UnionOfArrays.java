37ebbbea2a152c8ec0fe57792742cb68abd6e4f3
aryangupta1105 <husltegamingfanclub@gmail.com>
2025-06-17 09:12:34 +0530

    gfg 160days challenge - Unione of 2 Arrays - Hashing

---
+++ Hashing (Day 42 - 50)/Day 46 Union of Arrays with duplicates/UnionOfArrays.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.HashMap;
+
+public class UnionOfArrays {
+  static int findUnion(int a[], int b[]) {
+        HashMap<Integer, Integer> h  = new HashMap<>();
+        for(int i = 0; i<a.length; i++){
+            h.put(a[i] , h.getOrDefault(h.get(a[i]) , 0) + 1);
+        }
+        for(int i = 0; i<b.length; i++){
+            h.put(b[i] , h.getOrDefault(h.get(b[i]) , 0) + 1);
+        }
+        
+        return h.size();
+    }
+    public static void main(String[] args) {
+        int a[] = {1 , 2, 3, 4, 5};
+        int b[] = {1 , 2};
+
+        System.out.println("Unique Elements : " + findUnion(a, b));
+    }
+}
