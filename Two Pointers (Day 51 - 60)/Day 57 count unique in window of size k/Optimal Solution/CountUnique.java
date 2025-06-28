99fd0542b9bda086cbc8a82cfb6e4dc855459d36
aryangupta1105 <husltegamingfanclub@gmail.com>
2025-06-28 10:07:49 +0530

    gfg 160days challenge - Count Unique in window size k - Two Pointer

---
+++ Two Pointers (Day 51 - 60)/Day 57 count unique in window of size k/Optimal Solution/CountUnique.java
@@ -0,0 +1,39 @@
+import java.lang.*;
+import java.util.*;
+
+public class CountUnique {
+    
+    static ArrayList<Integer> countDistinct(int arr[], int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+
+        // Process the first window
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        ans.add(map.size());
+        System.out.println(map);
+        // Slide the window
+        for (int i = k; i < n; i++) {
+            // Remove the element going out of the window
+            int outElem = arr[i - k];
+            map.put(outElem, map.get(outElem) - 1);
+            if (map.get(outElem) == 0) {
+                map.remove(outElem);
+            }
+
+            System.out.println(map);
+
+            // Add the new element coming into the window
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            ans.add(map.size());
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 1, 3, 4, 2, 3};
+        System.out.println(countDistinct(arr, 4));
+    }
+}
